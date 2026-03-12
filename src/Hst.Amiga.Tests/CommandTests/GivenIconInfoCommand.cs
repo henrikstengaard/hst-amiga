436415374ed3d2952d161e722541ce207b8b03e3
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2026-03-12 12:43:03 +0100

    Add icon info command test with zero padded bytes

--- src/Hst.Amiga.Tests/CommandTests/GivenIconInfoCommand.cs
+++ src/Hst.Amiga.Tests/CommandTests/GivenIconInfoCommand.cs
@@ -13,6 +13,60 @@ namespace Hst.Amiga.Tests.CommandTests;
 
 public class GivenIconInfoCommand
 {
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(4)]
+    [InlineData(6)]
+    [InlineData(8)]
+    public async Task When_DiskObjectHasPaddedZeroes_ThenInfoIsRead(int zeroBytesCount)
+    {
+        // arrange - paths
+        var iconPath = $"{Guid.NewGuid()}.info";
+
+        // arrange - create disk object
+        var diskObject = DiskObjectHelper.CreateDiskInfo();
+        
+        // arrange - create icon data with disk object and zero padded bytes
+        byte[] iconData;
+        using (var iconStream = new MemoryStream())
+        {
+            await DiskObjectWriter.Write(diskObject, iconStream);
+            var zeroBytes = new byte[zeroBytesCount];
+            iconStream.Write(zeroBytes, 0, zeroBytes.Length);
+            iconData = iconStream.ToArray();
+        }
+        try
+        {
+            // arrange - write icon data
+            await File.WriteAllBytesAsync(iconPath, iconData);
+
+            // arrange - create icon info command
+            var messages = new List<string>(50);
+            var command = new IconInfoCommand(new NullLogger<IconInfoCommand>(), iconPath, true);
+            command.InformationMessage += (sender, message) =>
+            {
+                messages.Add(message);
+            };
+            
+            // act - execute icon info command
+            var result = await command.Execute(CancellationToken.None);
+            
+            // assert - result is successful
+            Assert.True(result.IsSuccess);
+            
+            // assert - messages not empty
+            Assert.NotEmpty(messages);
+        }
+        finally
+        {
+            if (File.Exists(iconPath))
+            {
+                File.Delete(iconPath);
+            }
+        }
+    }
+
     [Fact]
     public async Task When_DiskObjectHasColorIconWithOddSizeChunks_ThenInfoIsRead()
     {
